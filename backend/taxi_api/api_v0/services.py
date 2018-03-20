239ccb2ad71a2ad48833845af865b60df58f87cf
Vladimir Makhanov <makhanov.vladimir@gmail.com>
2018-03-20 22:05:57 +0500

    Move all other logics to service

--- backend/taxi_api/api_v0/services.py
+++ backend/taxi_api/api_v0/services.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from rest_framework import status
-from api_v0.models import Client, Order
-from api_v0.serializers import ClientSerializer, OrderSerializer
+from api_v0.models import Client, Driver, Car, Tariff, Order
+from api_v0.serializers import ClientSerializer, DriverSerializer, CarSerializer, TariffSerializer, OrderSerializer
+
+
+# Client section
 
 
 class ClientsService:
@@ -40,4 +43,172 @@ class SingleClientService:
     def delete_client_detail(self):
 
         self.client.delete()
+        return status.HTTP_204_NO_CONTENT
+
+
+# Driver section
+
+
+class DriversService:
+
+    def get_driver_list(self):
+        drivers = Driver.objects.all()
+        serializer = DriverSerializer(drivers, many=True)
+        return serializer.data
+
+    def post_driver_list(self, data):
+        serializer = DriverSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+
+class SingleDriverService:
+
+    def __init__(self, driver: Driver):
+        self.driver = driver
+
+    def get_driver_detail(self):
+        serializer = DriverSerializer(self.driver)
+        return serializer.data
+
+    def put_driver_detail(self, data):
+        serializer = DriverSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+    def delete_driver_detail(self):
+
+        self.driver.delete()
+        return status.HTTP_204_NO_CONTENT
+
+
+# Car section
+
+
+class CarsService:
+
+    def get_car_list(self):
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return serializer.data
+
+    def post_car_list(self, data):
+        serializer = CarSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+
+class SingleCarService:
+
+    def __init__(self, car: Car):
+        self.car = car
+
+    def get_car_detail(self):
+        serializer = CarSerializer(self.car)
+        return serializer.data
+
+    def put_car_detail(self, data):
+        serializer = CarSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+    def delete_car_detail(self):
+
+        self.car.delete()
+        return status.HTTP_204_NO_CONTENT
+
+
+# Tariff section
+
+
+class TariffsService:
+
+    def get_tariff_list(self):
+        tariffs = Tariff.objects.all()
+        serializer = TariffSerializer(tariffs, many=True)
+        return serializer.data
+
+    def post_tariff_list(self, data):
+        serializer = TariffSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+
+class SingleTariffService:
+
+    def __init__(self, tariff: Tariff):
+        self.tariff = tariff
+
+    def get_tariff_detail(self):
+        serializer = TariffSerializer(self.tariff)
+        return serializer.data
+
+    def put_tariff_detail(self, data):
+        serializer = TariffSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+    def delete_tariff_detail(self):
+
+        self.tariff.delete()
+        return status.HTTP_204_NO_CONTENT
+
+
+# Order section
+
+
+class OrdersService:
+
+    def get_order_list(self):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return serializer.data
+
+    def post_order_list(self, data):
+        serializer = OrderSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+
+class SingleOrderService:
+
+    def __init__(self, order: Order):
+        self.order = order
+
+    def get_order_detail(self):
+        serializer = OrderSerializer(self.order)
+        return serializer.data
+
+    def put_order_detail(self, data):
+        serializer = OrderSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data, status.HTTP_201_CREATED
+
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
+
+    def delete_order_detail(self):
+
+        self.order.delete()
         return status.HTTP_204_NO_CONTENT
--- backend/taxi_api/api_v0/views.py
+++ backend/taxi_api/api_v0/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from api_v0.models import Client, Driver, Car, Tariff, Order
-from api_v0.serializers import ClientSerializer, DriverSerializer, CarSerializer, TariffSerializer, OrderSerializer
-from api_v0.services import ClientsService, SingleClientService
+from api_v0.serializers import CarSerializer, TariffSerializer, OrderSerializer
+from api_v0.services import ClientsService, SingleClientService, DriversService, SingleDriverService, \
+    CarsService, SingleCarService, TariffsService, SingleTariffService, OrdersService, SingleOrderService
 
 # Create your views here.
 
@@ -57,17 +56,13 @@ def client_detail(request, pk, format=None):
 @api_view(['GET', 'POST'])
 def driver_list(request, format=None):
     if request.method == 'GET':
-        drivers = Driver.objects.all()
-        serializer = DriverSerializer(drivers)
-        return Response(serializer.data)
+        response = DriversService().get_driver_list()
+        return Response(response)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = DriverSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = DriversService().post_driver_list(data=data)
+        return Response(response)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -78,80 +73,31 @@ def driver_detail(request, pk, format=None):
         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = DriverSerializer(driver)
-        return Response(serializer.data)
+        response = SingleDriverService(driver).get_driver_detail()
+        return Response(response)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = DriverSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = SingleDriverService(driver).put_driver_detail(data=data)
+        return Response(response)
 
     elif request.method == 'DELETE':
-        driver.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-
-
-# Car section
-
-@api_view(['GET', 'POST'])
-def car_list(request, format=None):
-    if request.method == 'GET':
-        cars = Car.objects.all()
-        serializer = CarSerializer(cars, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def car_detail(request, pk, format=None):
-    try:
-        car = Car.objects.get(pk=pk)
-    except Car.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = CarSerializer(car)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        car.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        response = SingleDriverService(driver).delete_driver_detail()
+        return HttpResponse(response)
 
 
 # Tariff section
 
 @api_view(['GET', 'POST'])
-def tariff_list(request, format=None):
+def car_list(request, format=None):
     if request.method == 'GET':
-        tariffs = Tariff.objects.all()
-        serializer = TariffSerializer(tariffs, many=True)
-        return Response(serializer.data)
+        response = TariffsService().get_tariff_list()
+        return Response(response)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = TariffSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = TariffsService().post_tariff_list(data=data)
+        return Response(response)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -162,20 +108,17 @@ def tariff_detail(request, pk, format=None):
         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = TariffSerializer(tariff)
-        return Response(serializer.data)
+        response = SingleTariffService(tariff).get_tariff_detail()
+        return Response(response)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = TariffSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = SingleTariffService(tariff).put_tariff_detail(data=data)
+        return Response(response)
 
     elif request.method == 'DELETE':
-        tariff.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        response = SingleTariffService(tariff).delete_tariff_detail()
+        return HttpResponse(response)
 
 
 # Order section
@@ -183,17 +126,13 @@ def tariff_detail(request, pk, format=None):
 @api_view(['GET', 'POST'])
 def order_list(request, format=None):
     if request.method == 'GET':
-        orders = Order.objects.all()
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
+        response = OrdersService().get_order_list()
+        return Response(response)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = OrderSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = OrdersService().post_order_list(data=data)
+        return Response(response)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -204,17 +143,14 @@ def order_detail(request, pk, format=None):
         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = OrderSerializer(order)
-        return Response(serializer.data)
+        response = SingleOrderService(order).get_order_detail()
+        return Response(response)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = OrderSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = SingleOrderService(order).put_order_detail(data=data)
+        return Response(response)
 
     elif request.method == 'DELETE':
-        order.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        response = SingleOrderService(order).delete_order_detail()
+        return HttpResponse(response)
